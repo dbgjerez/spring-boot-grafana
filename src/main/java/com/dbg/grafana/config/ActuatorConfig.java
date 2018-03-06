3bade094c04c9eb2c519efda7eb340fdc39957b6
David Borrego Gutiérrez <dbgjerez@gmail.com>
2018-03-06 14:49:52 +0100

    versión completa con aplicación de prueba y datos por defecto

---
+++ src/main/java/com/dbg/grafana/config/ActuatorConfig.java
@@ -0,0 +1,26 @@
+package com.dbg.grafana.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ActuatorConfig {
+
+	// @Bean
+	// SpringBootMetricsCollector springBootMetricsCollector(Collection<Metric>
+	// publicMetrics) {
+	//
+	// SpringBootMetricsCollector springBootMetricsCollector = new
+	// SpringBootMetricsCollector(publicMetrics);
+	// springBootMetricsCollector.register();
+	//
+	// return springBootMetricsCollector;
+	// }
+
+	// @Bean
+	// ServletRegistrationBean<MetricsServlet> servletRegistrationBean() {
+	// DefaultExports.initialize();
+	// return new ServletRegistrationBean<MetricsServlet>(new MetricsServlet(),
+	// "/prometheus");
+	// }
+
+}
