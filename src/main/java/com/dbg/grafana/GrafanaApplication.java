ce5b1a3671b8cedb446091bbd6596f98a93e772a
David Borrego Gutiérrez <dbgjerez@gmail.com>
2018-03-06 09:21:50 +0100

    init

---
+++ src/main/java/com/dbg/grafana/GrafanaApplication.java
@@ -0,0 +1,12 @@
+package com.dbg.grafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafanaApplication.class, args);
+	}
+}
